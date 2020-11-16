892486c4767c9d39d553ecf57d8f9f40a8e9de71
cosven <yinshaowen241@gmail.com>
2020-11-16 10:46:27 +0000

    rename casectl to tipocket-ctl

---
+++ tipocket-ctl/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='tpctl',
+    version='0.1.dev0',
+    description='run tipocket testcase easier',
+    author='Cosven',
+    author_email='yinshaowen241@gmail.com',
+    packages=[
+        'casectl',
+    ],
+    package_data={
+        'casectl': []
+    },
+    python_requires=">=3.6",
+    url='https://github.com/cosven/tidb-testing/casectl',
+    keywords=['tidb', 'testing'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    install_requires=[
+        'click',
+    ],
+    extras_require={
+    },
+    tests_require=[],
+    entry_points={
+        'console_scripts': [
+            "tpctl=tpctl.__main__:main",
+        ]
+    },
+)
