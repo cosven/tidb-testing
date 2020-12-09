2f8bf5c44867ebf1a3a5ff0fd8905fc7025770bb
cosven <yinshaowen241@gmail.com>
2020-12-09 09:41:23 +0000

    tipocket-ctl: adapt to tipocket new feature, base64 config

--- tipocket-ctl/tpctl/params.py
+++ tipocket-ctl/tpctl/params.py
@@ -1,3 +1,5 @@
+import base64
+
 from tpctl.tidb_cluster import ComponentName, ComponentSpec, TidbClusterSpec
 
 
@@ -17,9 +19,18 @@ def parse_params(params):
             else:
                 value = 'false'
         # convert the config path to absolute form
+        # read the config file content and encode it to base64
         if key.endswith('_config'):
+            # value is a valid config file path
+            # read the content in config file and encode it with base64
+            # https://github.com/pingcap/tipocket/pull/330
             if value:
-                value = '/' + value
+                # TODO: catch FileNotExist error or validate the path somewhere
+                with open(value) as f:
+                    content = f.read()
+                content_bytes = bytes(content, 'utf-8')
+                b64content = base64.b64encode(content_bytes).decode('utf-8')
+                value = f'base64://{b64content}'
         case_params[key.replace('_', '-')] = value
     return case_params
 
@@ -32,8 +43,9 @@ def get_tidb_cluster_spec_from_params(params):
     components = []
     component_names = ComponentName.list_names()
     for component in component_names:
-        config_path = params[f'{component}_config']
-        # FIXME: the program must run in tipocket root directory
+        config_key = f'{component}_config'
+        config_path = params[config_key]
+        # NOTE: the program must run in tipocket root directory
         if config_path:
             with open(config_path) as f:
                 config = f.read()
