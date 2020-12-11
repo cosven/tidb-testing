41622c51c91aeb36facee85da06c61c5db42e5f6
Ofey Chan <ofey206@gmail.com>
2020-12-11 13:23:26 +0800

    Merged hello project for testing of test framework.

--- tipocket-ctl/tpctl/tests/data/hello/command.sh
+++
@@ -1,1 +0,0 @@
-tpctl prepare hello --build-image --run-time 2m --feature test-tpctl
--- tipocket-ctl/tpctl/tests/generate.sh
+++ tipocket-ctl/tpctl/tests/generate.sh
@@ -25,6 +25,12 @@ mkdir -p $TEST_DIR
 echo $TEST_COMMAND >> $TEST_DIR/command.sh
 
 cd $TIPOCKET_PATH
+
+if [ -d tpctl-build/ ]; then
+  rm -r tpctl-build/
+fi
+
 $TEST_COMMAND > $TEST_DIR/cli-output.txt
 cp -r tpctl-build/ $TEST_DIR/tpctl-build
+rm -rf tpctl-build/
 cd $TEST_ROOT
