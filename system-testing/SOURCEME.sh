55b1934bdbdd297ce0e74653b62d8cf85547e0f6
cosven <yinshaowen241@gmail.com>
2021-03-03 03:44:04 +0000

    several improvement

--- system-testing/SOURCEME.sh
+++ system-testing/SOURCEME.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 
-if [ -z "$ST_NAMESPACE" ]; then
-    echo "Please export environment variable ST_NAMESPACE"
-    false
-else
-    ns="$ST_NAMESPACE"
+ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+cd $ROOT
 
+source hack/lib.sh
+
+function st::setup_aliases() {
+    local ns="$1"
     unalias k
     unalias tkn
     unalias naglfar
     alias k="kubectl -n $ns"
     alias tkn="tkn -n $ns"
     alias naglfar="naglfar -n $ns"
-
     echo "You now have these aliases with -n option:"
     echo -e "\t\e[36mk=kubectl,tkn=tkn,naglfar=naglfar\e[0m"
-    echo -e "You can also try \e[34msource hack/bash_prompt.sh\e[0m"
-fi
+}
+
+# Command
+function st-use-ns() {
+    if [ -z "$1" ]; then
+        echo "Usage: st-use-ns NAMESPACE"
+        false
+    else
+        export ST_NAMESPACE="$1"
+        echo "System testing namespace is $1"
+        st::setup_aliases "$ST_NAMESPACE"
+        if [ $? -eq 0 ]; then
+           echo -e "You can also try \e[34msource hack/bash_prompt.sh\e[0m"
+        fi
+    fi
+}
+
+
+st-use-ns "$ST_NAMESPACE"
