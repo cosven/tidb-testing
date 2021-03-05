e40eb666fb0a8be244ee7fb03ecc71f4979f08c3
cosven <yinshaowen241@gmail.com>
2021-03-05 01:20:23 +0000

    st: update README

--- system-testing/README.md
+++ system-testing/README.md
@@ -9,4 +9,34 @@
 2. <https://google.github.io/styleguide/shellguide.html>
 2. <https://www.davidpashley.com/articles/writing-robust-shell-scripts/>
 
-## 常见操作记录
+# 操作手册
+
+## 快速上手
+
+快速部署一个 1pd-1db-3kv 的集群。
+
+1. 设置系统测试的关键环境变量：`export ST_NAMESPACE=st--xxx`。表明你要在哪个 namespace 下进行系统测试，
+   集群资源，tekton 任务都会创建在这个 ns 下。
+
+2. 运行 `source SOURCE.sh`，它会给常见命令设置 alias，其中关键的两个是 `k` 和 `tkn`。
+   ```
+   alias k="kubectl -n $ns"
+   alias tkn="tkn -n $ST_NAMESPACE"
+   ```
+
+3. 创建 serviceaccount 账号和相关 binding
+   ```
+   k apply -f rbac/
+   kubectl create clusterrolebinding "st-sa-binding-$ST_NAMESPACE" \
+     --clusterrole=system-testing --serviceaccount=$ST_NAMESPACE:system-testing
+   ```
+
+4. 部署 tasks 和 pipelines
+   ```
+   k apply -f pipelines/ -f tasks/
+   ```
+
+5. 启动 task 和 pipeline。以较简单的 pipeline `st--1pd1db3kv` 为例
+   ```
+   tkn-start-pipeline st--1pd1db3kv -p run-id=$ST_NAMESPACE
+   ```
--- system-testing/SOURCEME.sh
+++ system-testing/SOURCEME.sh
@@ -7,14 +7,21 @@ source hack/lib.sh
 
 function st::setup_aliases() {
     local ns="$1"
-    unalias k
-    unalias tkn
-    unalias naglfar
+    unalias k 2>%1
+    unalias tkn 2>%1
+    unalias naglfar 2>%1
+    unalias tkn-start-task 2>%1
+    unalias tkn-start-pipeline 2>%1
+
+    alias tkn="tkn -n $ST_NAMESPACE"
+    alias tkn-start-task="tkn -n $ST_NAMESPACE task start -s $ST_TEKTON_SA -w name=manifest,emptyDir="""
+    alias tkn-start-pipeline="tkn -n $ST_NAMESPACE pipeline start -s $ST_TEKTON_SA -w name=manifest,emptyDir="""
+    echo -e "Tekton tasks/pipelines all run in namespace \e[36m$ST_NAMESPACE\e[0m"
+
     alias k="kubectl -n $ns"
-    alias tkn="tkn -n $ns"
     alias naglfar="naglfar -n $ns"
     echo "You now have these aliases with -n option:"
-    echo -e "\t\e[36mk=kubectl,tkn=tkn,naglfar=naglfar\e[0m"
+    echo -e "\t\e[36mk=kubectl,naglfar=naglfar\e[0m"
 }
 
 # Command
@@ -24,7 +31,7 @@ function st-use-ns() {
         false
     else
         export ST_NAMESPACE="$1"
-        echo "System testing namespace is $1"
+        echo -e "System testing resources namespace is \e[36m$1\e[0m"
         st::setup_aliases "$ST_NAMESPACE"
         if [ $? -eq 0 ]; then
            echo -e "You can also try \e[34msource hack/bash_prompt.sh\e[0m"
@@ -32,5 +39,6 @@ function st-use-ns() {
     fi
 }
 
+ST_TEKTON_SA="system-testing"
 
 st-use-ns "$ST_NAMESPACE"
